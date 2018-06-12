11c62edfc4da464b5872ae9005790a91e885fc5c
ts-kim <zkm1989@naver.com>
2018-06-13 02:20:00 +0900

    Initial commit

---
+++ create_training_data.py
@@ -0,0 +1,39 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+from grabscreen import grab_screen
+import cv2
+import time
+from getkeys import key_check
+import os
+
+
+def keys_to_output(keys):
+    '''
+    Convert keys to a ...multi-hot... array
+
+    [A,W,D] boolean values.
+    '''
+    output = [0,0,0]
+    
+    if 'A' in keys:
+        output[0] = 1
+    elif 'D' in keys:
+        output[2] = 1
+    else:
+        output[1] = 1
+    return output
+
+file_name = 'training_data.npy'
+
+if os.path.isfile(file_name):
+    print('File exists, loading previous data!')
+    training_data = list(np.load(file_name))
+else:
+    print('File does not exist, starting fresh!')
+    training_data = []
+
